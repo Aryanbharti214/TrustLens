1b00f4c95b9c3f6968de002d5b252625d2f53ef5
Aryanbharti214 <bhartiaryan214@gmail.com>
2026-06-22 10:58:55 +0530

    deployment setup

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ def test_connection(provider: str):
 if __name__ == "__main__":
     import uvicorn
     # Run server on port 8000
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
